896b300f1e0c99465262f0d806f1a23e440510b5
Luiz Henrique <luizhenrique@pop-os.localdomain>
2022-03-28 18:20:09 -0300

    Primeira Versão

--- App/script.js
+++ App/script.js
@@ -5,12 +5,12 @@ logado.innerHTML = `Olá ${userLogado.nome}`
 
 if (localStorage.getItem("token") == null) {
     alert("Você precisa estar logado para estar nessa página")
-    window.location.href = "../Login/index.html"
+    window.location.href = "Login-App/index.html"
 
 }
 
 function sair() {
     localStorage.removeItem('token')
     localStorage.removeItem('userLogado')
-    window.location.href = "./index.html"
+    window.location.href = "Login-App/index.html"
 }
